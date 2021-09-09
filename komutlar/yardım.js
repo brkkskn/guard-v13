1c48d1526b9dd813c1e298010b03a20b9916a3a1
Glitch (rvs-guard-v12) <none>
2021-09-09 22:48:24 +0000

    🌴⛺️ Checkpoint ./komutlar/yardım.js:21238376/2139

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -7,7 +7,8 @@ exports.run = async(client, message, args) => {
 
         const jkood = new Discord.MessageEmbed()
              .setColor('#fff000')
-             .addField(`İşte Komutlarım!`, `
+             .setAuthor(`Yardım Komutları`, client.user.avatarURL())         
+             .addField(``,`
              ${prefix}antiraid aç** Spam ve DDOS saldırılarını önler.
              ${prefix}capsengel aç** Sunucuda Capslock ile Yazılmasını önler.
              ${prefix}emojikoruma aç** Başka bir sunucunun emojileri kullanmasına engel olur
@@ -17,13 +18,14 @@ exports.run = async(client, message, args) => {
              ${prefix}modlog #kanal** Gelen - Giden kanalı belirler.
              ${prefix}reklamengel aç** Reklam atılmasına engel olur.
              ${prefix}rolkoruma aç** Rollerin silinmesini önler.
-             ${prefix}sohbet-aç**
-             ${prefix}sohbet-kapat**
-             ${prefix}sil-üye @üye miktar**
-             ${prefix}sil miktar**
-             ${prefix}yavaşmod süre**
-             ${prefix}ban @üye sebep**
-             ${prefix}kick @üye sebep**         
+             ${prefix}sohbet-aç** Belirlediğiniz kanalın sohbetini açar.
+             ${prefix}sohbet-kapat** Belirledğiniz kanalın sohbetini kapatır.
+             ${prefix}sil-üye @üye miktar** Belirli miktarda üyenin mesajlarını siler.
+             ${prefix}sil miktar** Sohbetteki mesajları siler.
+             ${prefix}yavaşmod süre** Yavaşmod.
+             ${prefix}ban @üye sebep** Üyeyi banlar.
+             ${prefix}kick @üye sebep**
+             
              `)
              .setFooter(`${message.author.username} Tarafından istendi.`, message.author.avatarURL())
             
